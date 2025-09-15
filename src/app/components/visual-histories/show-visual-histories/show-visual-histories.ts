0c8eda46788da4b3bbe71d2b14dda0b606314ae1
FabioRg06 <romero.gomez.fabio@gmail.com>
2025-09-15 07:40:29 -0500

    changes on visual histories to include patient names instead of IDs.

--- src/app/components/visual-histories/show-visual-histories/show-visual-histories.ts
+++ src/app/components/visual-histories/show-visual-histories/show-visual-histories.ts
@@ -5,7 +5,8 @@ import { VisualHistoryI } from '../../../models/visual-history';
 import { ButtonModule } from 'primeng/button';
 import { RouterModule } from '@angular/router';
 import { VisualHistoryService } from '../../../services/visual-history.service';
-
+import { PatientService } from '../../../services/patient.service';
+import { PatientI } from '../../../models/patient';
 
 @Component({
   selector: 'app-show-visual-histories',
@@ -16,10 +17,19 @@ import { VisualHistoryService } from '../../../services/visual-history.service';
 })
 export class ShowVisualHistories {
   visualHistories: VisualHistoryI[] = []
+  patients: PatientI[] = [];
 
-  constructor(private visualHistoryService: VisualHistoryService) {
+  constructor(private visualHistoryService: VisualHistoryService, 
+            private patientService: PatientService) {
     this.visualHistoryService.visualhistories$.subscribe(visualhistories => {
       this.visualHistories = visualhistories; 
     });
+    this.patientService.patients$.subscribe(data => {
+      this.patients = data;
+    });
+  }
+  getPatientName(id: number): string {
+    const patient = this.patients.find(p => p.id === id);
+    return patient ? patient.name : 'Paciente no encontrado';
   }
 }
