c3b9b60fd236967907107f200bc408df5266329d
FabioRg06 <romero.gomez.fabio@gmail.com>
2025-09-15 08:01:26 -0500

    changes on appointments component to display patient and optometrist names instead of IDs.

--- src/app/components/appointments/show-appointments/show-appointments.ts
+++ src/app/components/appointments/show-appointments/show-appointments.ts
@@ -4,7 +4,11 @@ import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { RouterModule } from '@angular/router';
 import { AppointmentI } from '../../../models/appointment';
+import { PatientI } from '../../../models/patient';
+import { OptometristI } from '../../../models/ optometrist';
 import { AppointmentService } from '../../../services/appointment.service';
+import { OptometristService } from '../../../services/optometrist.service';
+import { PatientService } from '../../../services/patient.service';
 
 
 @Component({
@@ -16,10 +20,30 @@ import { AppointmentService } from '../../../services/appointment.service';
 })
 export class ShowAppointments {
   appointments: AppointmentI[] = [];
+  patients: PatientI[] = [];
+  optometrists: OptometristI[] = [];
 
-  constructor(private appointmentService: AppointmentService) {
+  constructor(private appointmentService: AppointmentService,
+    private patientService: PatientService,
+    private optometristService: OptometristService
+  ) {
     this.appointmentService.appointments$.subscribe(appointments => {
       this.appointments = appointments;
     });
+    this.patientService.patients$.subscribe(data => {
+      this.patients = data;
+    });
+    this.optometristService.optometrists$.subscribe(data => {
+      this.optometrists = data;
+    });
+  }
+  getPatientName(id: number): string {
+    const patient = this.patients.find(p => p.id === id);
+    return patient ? patient.name : 'Paciente no encontrado';
+  }
+
+  getOptometristName(id: number): string {
+    const opto = this.optometrists.find(o => o.id === id);
+    return opto ? opto.name : 'Optometrista no encontrado';
   }
 }
